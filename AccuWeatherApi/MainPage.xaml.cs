307d75415087e2a7653911121e57e534137307c8
h <h@DESKTOP-IUGD2N2>
2020-08-24 11:52:48 +0700

    AccuWeatherApi

--- AccuWeatherApi/MainPage.xaml.cs
+++ AccuWeatherApi/MainPage.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using WeatherDemo.Models;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -12,12 +11,11 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace WeatherDemo
+namespace AccuWeatherApi
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -27,18 +25,6 @@ namespace WeatherDemo
         public MainPage()
         {
             this.InitializeComponent();
-        }
-
-        private async void Page_Loaded(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-              
-            }
-            catch (Exception ex)
-            {
-                LocationTextBlock.Text = "Khong lay duoc toa do";
-            }
         }
     }
 }
---
+++ AccuWeatherApi/Models/WeatherJson.cs
@@ -0,0 +1,40 @@
+﻿using SharpDX;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccuWeatherApi.Models
+{ public class Temerature
+    {
+        public string Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+    public class WeatherJson
+    {
+        public string DataTime { get; set; }
+        public int EpochDateTime { get; set; }
+        public string WeatherIcon { get; set; }
+        public string IconPhrase { get; set; }
+        public string IsDaylight { get; set; }
+        public Temerature Temerature { get; set; }
+        public int PrecipitationProbability { get; set; }
+        public string MobileLink { get; set; }
+        public string Link { get; set; }
+    }
+    public async static Task<List<WeatherJson>> GetJson(string url)
+    {
+        var http = new HttpClient();
+        var response = await http.GetAsync(url); // nhan data tu json
+        var result = await response.Content.ReadAsStringAsync();
+        var serializer = new DataContractJsonSerializer(typeof(List<WeatherEachDay>)); //dong bo 
+        var ms = new MemoryStream(Encoding.UTF8.GetBytes(result)); // format de khong bi loi 
+        var Value = serializer.ReadObject(dataSteam) as List<WeatherJson>; // Doc ra giu lieu data
+        return Value;
+    }
+}
